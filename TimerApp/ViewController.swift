abd95d9b6e512160908b2db94e80fa6c0f088a8c
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-06 11:09:24 -0600

    conformed to delegate in view controller and made extension

--- TimerApp/ViewController.swift
+++ TimerApp/ViewController.swift
@@ -22,13 +22,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         napTimer.startTimer(20)
+        napTimer.delegate = self
     }
 
+    // MARK: - Custom Methods
+    func updateLabelAndButton() {
+        timerLabel.text = napTimer.timeLeftAsString()
+        
+        var buttonTitle = "Stop"
+        var buttonColor: UIColor
+        
+        if napTimer.isOn {
+            buttonTitle = "Stop"
+            buttonColor = .red
+        } else {
+            buttonColor = .white
+            buttonTitle = "Start"
+        }
+        
+        timerButton.setTitle(buttonTitle, for: .normal)
+        timerButton.setTitleColor(buttonColor, for: .normal)
+    }
     
     
     // MARK: - Actions
     @IBAction func timerButtonTapped(_ sender: UIButton) {
+        if napTimer.isOn {
+            napTimer.stopTimer()
+        } else {
+            napTimer.startTimer(8)
+        }
     }
     
 }
 
+extension ViewController: NapTimerDelegate {
+    func timerSecondTicked() {
+        updateLabelAndButton()
+    }
+    
+    func timerStopped() {
+        updateLabelAndButton()
+    }
+    
+    func timerCompleted() {
+        
+    }
+    
+    
+}
